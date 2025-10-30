7a55aa719f155afed183e8b6061c5931506320d4
Janaína Bezerra <janainabezerra.ti@gmail.com>
2025-10-30 15:05:04 -0300

    Conserto de erro de digitação

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,14 @@ function App(){
 <AuthProvider>
   <BrowserRouter>
     <Navbar />
-    <div className='min-h-[80vRh]'>
+    <div className='min-h-[80vh]'>
     <Routes>
       <Route path='/' element={<Login />} />
       <Route path="/home" element={<Home />} />
       <Route path="/cadastro" element={<Cadastro />} />
       <Route path="/temas" element={<ListaTemas/>} />
-      <Route path="/cadastrartema" element={<FormTema/>} />
-      <Route path="/editartema/:id" element={<FormTema/>} />
+      <Route path="/cadastrartema" element={<FormTema />} />
+      <Route path="/editartema/:id" element={<FormTema />} />
       <Route path="/deletartema/:id" element={<DeletarTema />} />
     </Routes>
     </div>
